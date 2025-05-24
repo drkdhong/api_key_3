4e78b7f8c1eea6c8b99426181bd53e2d2ab8db6e
aiforkorea <aiforkorea@gmail.com>
2025-05-24 20:53:21 +0900

    port 변경

--- app/run.py
+++ app/run.py
@@ -2,4 +2,5 @@ from app import create_app
 app =create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render가 포트 넘버 환경변수로 줍니다!
+    app.run(host="0.0.0.0", port=port, debug=False)
